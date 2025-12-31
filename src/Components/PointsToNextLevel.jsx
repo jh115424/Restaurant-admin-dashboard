8e9da9f30664dc2d2cbdf3fc5bf74a37b2347aee
jennifer hodge <jennifer.hodge@russdarrow.com>
2025-12-31 07:32:49 -0600

    Employee dashboard is done

--- src/Components/PointsToNextLevel.jsx
+++ src/Components/PointsToNextLevel.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import "./pointsToNextLevel.css";
 import bronzeShield from "/public/bronzeShield.png";
@@ -21,58 +20,54 @@ export default function PointsToNextLevel() {
 
   const [membershipStatus, setMembershipStatus] = React.useState("");
   const [nextTierPoints, setNextTierPoints] = React.useState(0);
-  const [memberActivityPoints, setMemberActivityPoints] = React.useState("");
-  const [submittedPoints, setSubmittedPoints] = React.useState(false);
 
-  /* VISIT */
   const employeeHandleClick = (id) => {
     setGuestData((prev) =>
       prev.map((guest) => {
         if (guest.id !== id) return guest;
-
         const newPointTotal = guest.points + 10;
         applyTierLogic(newPointTotal);
-
         return { ...guest, points: newPointTotal };
       })
     );
   };
 
-  /* ADJUST */
   const adjustHandleClick = (id) => {
     setGuestData((prev) =>
       prev.map((guest) => {
         if (guest.id !== id) return guest;
-
         const newPointTotal = Math.max(0, guest.points - 100);
         applyTierLogic(newPointTotal);
-
         return { ...guest, points: newPointTotal };
       })
     );
   };
 
-  /* SHARED TIER LOGIC */
+  const correctionHandleClick = (id) => {
+    setGuestData((prev) =>
+      prev.map((guest) => {
+        if (guest.id !== id) return guest;
+        const newPointTotal = Math.max(0, guest.points + 50);
+        applyTierLogic(newPointTotal);
+        return { ...guest, points: newPointTotal };
+      })
+    );
+  };
+
   const applyTierLogic = (newPointTotal) => {
     if (newPointTotal <= bronze) {
       setMembershipStatus("bronze");
       setNextTierPoints(silver - newPointTotal);
-      setMemberActivityPoints("bronze");
     } else if (newPointTotal <= silver) {
       setMembershipStatus("silver");
       setNextTierPoints(gold - newPointTotal);
-      setMemberActivityPoints("silver");
     } else if (newPointTotal <= gold) {
       setMembershipStatus("gold");
       setNextTierPoints(platinum - newPointTotal);
-      setMemberActivityPoints("gold");
     } else {
       setMembershipStatus("platinum");
       setNextTierPoints(0);
-      setMemberActivityPoints("platinum");
     }
-
-    setSubmittedPoints(true);
   };
 
   return (
@@ -87,51 +82,117 @@ export default function PointsToNextLevel() {
       </div>
 
       <div className="guestPointCards">
-        {guestData.map((guest) => (
-          <div className="cardBlock" key={guest.id}>
-            <div className={`${guest.tier}Card`}>
-              <p className="name">{guest.name}</p>
-
-              <div className="pointAmount">
-                <p className="points">{guest.points}</p>
-              </div>
-
-              <div className={`${guest.tier}PointShield`}>
-                <img
-                  src={
-                    guest.tier === "bronze"
-                      ? bronzeShield
-                      : guest.tier === "silver"
-                      ? silverShield
-                      : guest.tier === "gold"
-                      ? goldShield
-                      : platinumShield
-                  }
-                  alt={`${guest.tier}-shield`}
-                  className={`${guest.tier}CardLogo`}
-                />
-              </div>
+        {/* CARD 1 - Erik Johnson */}
+        <div className="cardBlock">
+          <div className="bronzeCard">
+            <p className="name">{guestData[0].name}</p>
+            <div className="pointAmount">
+              <p className="points">{guestData[0].points}</p>
             </div>
-
-            <div className="cardActions">
-              <button
-                type="button"
-                onClick={() => employeeHandleClick(guest.id)}
-              >
-                Visit
-              </button>
-
-              <button
-                type="button"
-                onClick={() => adjustHandleClick(guest.id)}
-              >
-                Adjust
-              </button>
-
-              <button>Correction</button>
+            <div className="bronzePointShield">
+              <img
+                src={bronzeShield}
+                alt="bronze-shield"
+                className="bronzeCardLogo"
+              />
             </div>
           </div>
-        ))}
+          <div className="cardActions">
+            <button type="button" onClick={() => employeeHandleClick(1)}>
+              Visit
+            </button>
+            <button type="button" onClick={() => adjustHandleClick(1)}>
+              Adjust
+            </button>
+            <button type="button" onClick={() => correctionHandleClick(1)}>
+              Correction
+            </button>
+          </div>
+        </div>
+
+        {/* CARD 2 - Jennifer Hodge */}
+        <div className="cardBlock">
+          <div className="silverCard">
+            <p className="name">{guestData[1].name}</p>
+            <div className="pointAmount">
+              <p className="points">{guestData[1].points}</p>
+            </div>
+            <div className="silverPointShield">
+              <img
+                src={silverShield}
+                alt="silver-shield"
+                className="silverCardLogo"
+              />
+            </div>
+          </div>
+          <div className="cardActions">
+            <button type="button" onClick={() => employeeHandleClick(2)}>
+              Visit
+            </button>
+            <button type="button" onClick={() => adjustHandleClick(2)}>
+              Adjust
+            </button>
+            <button type="button" onClick={() => correctionHandleClick(2)}>
+              Correction
+            </button>
+          </div>
+        </div>
+
+        {/* CARD 3 - Rebecca Chamberlain */}
+        <div className="cardBlock">
+          <div className="goldCard">
+            <p className="name">{guestData[2].name}</p>
+            <div className="pointAmount">
+              <p className="points">{guestData[2].points}</p>
+            </div>
+            <div className="goldPointShield">
+              <img
+                src={goldShield}
+                alt="gold-shield"
+                className="goldCardLogo"
+              />
+            </div>
+          </div>
+          <div className="cardActions">
+            <button type="button" onClick={() => employeeHandleClick(3)}>
+              Visit
+            </button>
+            <button type="button" onClick={() => adjustHandleClick(3)}>
+              Adjust
+            </button>
+            <button type="button" onClick={() => correctionHandleClick(3)}>
+              Correction
+            </button>
+          </div>
+        </div>
+
+        {/* CARD 4 - Sarah Johnson */}
+        <div className="cardBlock">
+          <div className="platinumCard">
+            <p className="name">{guestData[3].name}</p>
+            <div className="pointAmount">
+              <p className="points">{guestData[3].points}</p>
+            </div>
+            <div className="platinumPointShield">
+              <img
+                src={platinumShield}
+                alt="platinum-shield"
+                className="platinumCardLogo"
+              />
+            </div>
+          </div>
+          <div className="cardActions">
+            <button type="button" onClick={() => employeeHandleClick(4)}>
+              Visit
+            </button>
+            <button type="button" onClick={() => adjustHandleClick(4)}>
+              Adjust
+            </button>
+            <button type="button" onClick={() => correctionHandleClick(4)}>
+              Correction
+            </button>
+          </div>
+        </div>
       </div>
 
       <div className="underline"></div>
